630a47b5ccc3f283b5b7eff498474f2c2d056446
alex-smith <azphreal19@protonmail.com>
2018-01-06 11:32:58 +1100

    Main function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
+extern crate rand;
+extern crate termion;
+
 mod board;
 mod tests;
 mod game;
 
+use std::io;
+use termion::raw::IntoRawMode;
+
 fn main() {
-    game::run();
+    let stdout = io::stdout();
+    let mut stdout = stdout.lock();
+    let stdin = io::stdin();
+    let stdin = stdin.lock();
+    let stderr = io::stderr();
+    let mut stderr = stderr.lock();
+
+    let stdout = stdout.into_raw_mode().unwrap();
+    game::init(stdin, stdout);
 }
